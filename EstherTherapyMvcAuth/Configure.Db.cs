c9dbd621c45b264282fc9db23d200d89b14c6039
arxangeldaniel2017 <arxangeldaniel@gmail.com>
2024-07-31 00:27:51 +0300

    admin ui and locode working

--- EstherTherapyMvcAuth/Configure.Db.cs
+++ EstherTherapyMvcAuth/Configure.Db.cs
@@ -8,51 +8,38 @@ using ServiceStack.OrmLite;
 
 [assembly: HostingStartup(typeof(EstherTherapyMvcAuth.ConfigureDb))]
 
-namespace EstherTherapyMvcAuth
+namespace EstherTherapyMvcAuth;
+
+public class ConfigureDb : IHostingStartup
 {
-    // Example Data Model
-    public class MyTable
-    {
-        [AutoIncrement]
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices((context, services) => {
+            services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
+                context.Configuration.GetConnectionString("DefaultConnection")
+                ?? ":memory:",
+                SqliteDialect.Provider));
+        })
+        .ConfigureAppHost(appHost => {
+            // Enable built-in Database Admin UI at /admin-ui/database
+            appHost.Plugins.Add(new AdminDatabaseFeature());
 
-    public class ConfigureDb : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder) => builder
-            .ConfigureServices((context, services) => {
-                services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
-                    context.Configuration.GetConnectionString("DefaultConnection")
-                    ?? ":memory:",
-                    SqliteDialect.Provider));
-            })
-            .ConfigureAppHost(afterConfigure:appHost => {
-                appHost.ScriptContext.ScriptMethods.Add(new DbScriptsAsync());
-
-                // Create non-existing Table and add Seed Data Example
-                using var db = appHost.Resolve<IDbConnectionFactory>().Open();
-                if (db.CreateTableIfNotExists<MyTable>())
+            using var db = appHost.Resolve<IDbConnectionFactory>().Open();
+            if (db.CreateTableIfNotExists<Booking>())
+            {
+                // Seed data
+                db.Insert(new Booking
                 {
-                    db.Insert(new MyTable { Name = "Seed Data for new MyTable" });
-                }
-
-                if (db.CreateTableIfNotExists<Booking>())
-                {
-                    // Seed data
-                    db.Insert(new Booking
-                    {
-                        Name = "Test",
-                        Cost = 123,
-                        RoomNumber = 321,
-                        RoomType = RoomType.Queen,
-                        Notes = "Testing more",
-                        BookingStartDate = new DateTime(2022, 1, 1),
-                        BookingEndDate = new DateTime(2022, 1, 5),
-                        CreatedBy = "daniel",
-                        ModifiedBy = "daniel",
-                    });
-                }
-            });
-    }
+                    Name = "Test",
+                    Cost = 123,
+                    RoomNumber = 321,
+                    RoomType = RoomType.Queen,
+                    Notes = "Testing more",
+                    BookingStartDate = new DateTime(2022, 1, 1),
+                    BookingEndDate = new DateTime(2022, 1, 5),
+                    CreatedBy = "daniel",
+                    //CreatedDate = DateTime.Now,
+                    ModifiedBy = "daniel",
+                });
+            }
+        });
 }
